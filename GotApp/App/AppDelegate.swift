86cc36a159b0ca1d42ab15b74fb9b7362af8885d
borisbolsh <bolsh.bo@yandex.ru>
2022-01-18 08:42:11 +0300

    first commit

---
+++ GotApp/App/AppDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        // TODO: Change to coordinator
+        let navVC = UINavigationController(rootViewController:ViewController())
+        
+        window?.rootViewController = navVC
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+    
+}
+
